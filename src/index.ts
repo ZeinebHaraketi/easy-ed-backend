5b2a6c512d9e70c7aa14c4e8fef25b92237eab40
Zeineb Haraketi <zeinebharaketi298@gmail.com>
2026-01-16 21:22:15 +0000

    Implementing All routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+
+import subjectsRouter from "./routes/subjects";
+import usersRouter from "./routes/users";
+import classesRouter from "./routes/classes";
+import departmentsRouter from "./routes/departments";
+import statsRouter from "./routes/stats";
+import enrollmentsRouter from "./routes/enrollments";
+
+
+const PORT = process.env.PORT || 8000;
+
+
+
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Easy-Ed backend is running." });
-});
 
-const PORT = process.env.PORT || 8000;
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // React app URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // allow cookies
+  })
+);
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.json({ message: "Easy-Ed backend is running." });
+// });
+
+
+app.use("/api/subjects", subjectsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/classes", classesRouter);
+app.use("/api/departments", departmentsRouter);
+app.use("/api/stats", statsRouter);
+app.use("/api/enrollments", enrollmentsRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
